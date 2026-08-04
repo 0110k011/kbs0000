484c1f90191a52f518b049cbb9b71c2db3a673d3
0110k011 <gmateust@gmail.com>
2026-08-04 19:18:23 -0300

    fix JPA

--- src/main/java/com/kbs0000/project/modules/transaction/TransactionEntity.java
+++ src/main/java/com/kbs0000/project/modules/transaction/TransactionEntity.java
@@ -1,4 +1,23 @@
 package com.kbs0000.project.modules.transaction;
 
-public class TransactionEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transaction")
+@Getter
+@Setter
+public class TransactionEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 }
